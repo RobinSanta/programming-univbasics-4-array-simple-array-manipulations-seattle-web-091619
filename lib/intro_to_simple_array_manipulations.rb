f62568b7a8cc90cbf0f410d8daf07cde159857c7
Robinson Santana Silverio <robinsntn@gmail.com>
2019-09-18 02:22:39 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -26,7 +26,8 @@ def using_concat(my_favorite_things, more_favs)
   my_favorite_things.concat(more_favs)
 end
 
-def using_insert()
+def using_insert(list_of_programming_languages, another_language)
+  
 end
 
 def using_uniq(haircuts)
