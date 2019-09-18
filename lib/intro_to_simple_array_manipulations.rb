8cc2048ff0bbf69216bcedb9dcfd2690810fdd68
Robinson Santana Silverio <robinsntn@gmail.com>
2019-09-18 02:17:34 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -23,8 +23,16 @@ def shift_with_args(ice_cream_brands)
 end
 
 def using_concat(my_favorite_things, more_favs)
-  
+  my_favorite_things.concat(more_favs)
+end
 
+def using_insert(list_of_programming_languages, another_language)
+  list_of_programming_languages.insert(another_language)
+end
+
+def using_uniq(haircuts)
+  haircuts.uniq
+end
 
 
 
