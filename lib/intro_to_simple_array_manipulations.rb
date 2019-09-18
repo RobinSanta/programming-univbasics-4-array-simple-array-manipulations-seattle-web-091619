244fe69fcb1d774f8417f2ff6c76e272b9777de9
Robinson Santana Silverio <robinsntn@gmail.com>
2019-09-18 02:02:04 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -2,6 +2,7 @@ def using_push(colors_of_the_rainbow, next_color)
   colors_of_the_rainbow.push(next_color)
 end
 
-def usning_unshift(bouroughs_in_nyc, new_neighborhood)
+def using_unshift(bouroughs_in_nyc, new_neighborhood)
   bouroughs_in_nyc.unshift(new_neighborhood)
 end
+
