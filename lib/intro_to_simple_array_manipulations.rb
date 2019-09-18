cb38d66bc36b28c003884a20894fbd4cab6acbce
Robinson Santana Silverio <robinsntn@gmail.com>
2019-09-18 02:34:19 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -38,11 +38,15 @@ def using_flatten(instruments)
   instruments.flatten
 end
 
-def using_delete(famous_robots, integer)
-  famous_robots.delete(integer)
+def using_delete(array, string)
+  array.delete(string)
 end
 
+def using_delete_at(famous_robots, 2)
+  famous_robots.delete(2)
+end
 
+  
 
 
 
